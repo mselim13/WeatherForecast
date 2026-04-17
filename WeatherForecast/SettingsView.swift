d4cafd99c0b4fa1978fa1d2725cdaf3ce5eb2f43
mselim13 <mselimerdal@gmail.com>
2026-04-17 19:55:29 +0300

    küçük düzenlemeler yapıldı.

--- WeatherForecast/SettingsView.swift
+++ WeatherForecast/SettingsView.swift
@@ -1,15 +1,66 @@
-//
-//  SettingsView.swift
-//  WeatherForecast
-//
-//  Created by Mustafa Selim Erdal on 26.01.2026.
-//
-
 import SwiftUI
 
 struct SettingsView: View {
+    @Environment(\.dismiss) private var dismiss
+    @State private var currentCondition = "sunny"
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ZStack {
+                Image(currentCondition)
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea()
+                    .blur(radius: 20)
+
+                ScrollView {
+                    VStack(spacing: 14) {
+
+                        SettingRow(title: "Birim: °C / °F")
+                        SettingRow(title: "Bildirimler")
+                        SettingRow(title: "Tema")
+
+                    }
+                    .padding()
+                }
+                
+                .padding()
+            }
+            .navigationTitle("Ayarlar")
+            .navigationTitle(Text("Ayarlar"))
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: "xmark")
+                            
+                            .font(.title2.bold())
+                            .foregroundStyle(Color(.systemGray))
+                            .padding(5)
+                            
+                            
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct SettingRow: View {
+    let title: String
+
+    var body: some View {
+        HStack {
+            Text(title)
+                .font(.body)
+            Spacer()
+
+            Image(systemName: "chevron.right")
+                .foregroundStyle(.secondary)
+        }
+        .padding()
+        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 15))
     }
 }
 
